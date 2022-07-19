293aa33a37bb27f0ae1cef7049dd556d98cb9b62
Kim-Hyerin <kor99lyn@naver.com>
2022-07-20 04:02:57 +0900

    04-3 컴포넌트에 key props 추가하기
    - map() 함수로 만든 컴포넌트에 key props (id 값) 추가
    - img에 alt 속성 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
 /* 컴포넌트명은 대문자로 시작 */
@@ -5,29 +6,34 @@ function Food({ name, picture }) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'salmon',
     image: 'http://m.62life.com/images/gdimg/71560001_main(0).jpg',
   },
   {
+    id: 2,
     name: 'sushi',
     image: 'https://previews.123rf.com/images/hongee/hongee1608/hongee160800010/61684605-%EB%AA%A8%EB%93%AC-%EC%8A%A4%EC%8B%9C-%EC%84%B8%ED%8A%B8.jpg',
   },
   {
+    id: 3,
     name: 'bubble tea',
     image: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAPdgnJ.img?h=552&w=750&m=6&q=60&u=t&o=f&l=f',
   },
   {
+    id: 4,
     name: 'Sausage and rice cake',
     image: 'https://m.yoriking.com/web/product/medium/skewer/sotteok8p_s1.jpg',
   },
   {
+    id: 5,
     name: 'Duck Bulgogi',
     image: 'https://mblogthumb-phinf.pstatic.net/MjAxODEyMjhfMzEg/MDAxNTQ1OTUxMDIwNTEw.GDZI_JZSYhcc6fUApPepYN-tpUAOlZ84bJxIPdIQKmUg.LzbUzml9XCoLiwi6sL4RsmjlEGidxNoCbaC93XwLDXQg.JPEG.sable8270/%EB%B6%80%EC%82%B0_%EA%B0%80%EC%95%BC%EA%B3%B5%EC%9B%90_%EC%98%A4%EB%A6%AC%EA%B3%A0%EA%B8%B0_%EB%A7%9B%EC%A7%91_%EB%83%89%EC%88%98%ED%83%95%EA%B0%80%EB%93%A014.jpg?type=w800',
   }
@@ -36,7 +42,9 @@ const foodILike = [
 function App() {
   return (
   <div>
-    {foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))}
+    {foodILike.map(dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image} />
+    ))}
   </div>
   );
 }
